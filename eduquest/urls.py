23d3992dbdd83a0fddb7d0df8a7ff285e1404cc6
earlyjohnny <earljohngguimoc@gmail.com>
2026-04-09 14:06:46 +0800

    Configure root URL to redirect index to login page

--- eduquest/urls.py
+++ eduquest/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for EduQuest project.
+URL configuration for eduquest project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect('login'), name='index'),
+    path('accounts/', include('accounts.urls')),
 ]
