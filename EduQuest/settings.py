b0126e3f7fa5d669dc258dd7fdd7a97558dec4e7
earlyjohnny <earljohngguimoc@gmail.com>
2026-04-09 12:10:31 +0800

    Updated app structure to match final project mapping

--- EduQuest/settings.py
+++ EduQuest/settings.py
@@ -38,12 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #apps
-    'users',
+    'accounts',
     'organizations',
     'events',
-    'documents',
-    'equipment',
-    'announcements'
+    'registrations',
+    'finances',
+    'communications',
 ]
 
 MIDDLEWARE = [
