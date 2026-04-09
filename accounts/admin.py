38263a45b08f70f10d90b2fe69658b827ffae3d6
earlyjohnny <earljohngguimoc@gmail.com>
2026-04-09 14:04:28 +0800

    Register accounts models to Django admin with proper password hashing

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Profile, Role, Membership
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Profile)
+admin.site.register(Role)
+admin.site.register(Membership)
