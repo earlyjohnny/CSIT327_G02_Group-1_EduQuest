c37467099590b14688bfff67e7492589c0ae3386
earlyjohnny <earljohngguimoc@gmail.com>
2026-04-17 22:35:26 +0800

    Configure addNewRegistration route and app-level namespacing

--- registrations/urls.py
+++ registrations/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.RegistrationsView.as_view(), name='registrations'),
+    path('', views.RegistrationsView.as_view(), name='registrations_index'),
+    path('addNewRegistration/', views.AddRegistrationView.as_view(), name='add_registration'),
 ]
