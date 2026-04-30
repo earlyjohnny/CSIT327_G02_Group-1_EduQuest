1e9d0fa5b7b80f2d5a89c928eca357773b74a48e
earlyjohnny <earljohngguimoc@gmail.com>
2026-05-01 06:46:21 +0800

    Registered Registration and Attendance models

--- registrations/admin.py
+++ registrations/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from registrations.models import Registration, Attendance
+
 # Register your models here.
+admin.site.register(Registration)
+admin.site.register(Attendance)
+
